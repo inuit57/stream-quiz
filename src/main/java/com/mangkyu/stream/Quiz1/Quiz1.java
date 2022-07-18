d44e870cbe35f7e3c5f8753c41fe21700264fb46
inuit57 <inuit57@naver.com>
2022-07-18 11:29:15 +0900

    퀴즈 1 학습

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -4,24 +4,57 @@ import com.opencsv.CSVReader;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz1 {
 
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        Map<String, Integer> result = csvLines.stream()
+                .map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+
+//        for (String s : result.keySet()) {
+//            System.out.println(s+":"+ result.get(s));
+//        }
+
+        return result;
     }
 
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        return csvLines.stream()
+                .filter(line->line[0].startsWith("정"))
+                .map(line -> line[1].replaceAll("\\s",""))
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                .collect(Collectors.toMap(hobby->hobby, hobby->1, (oldValue,newValue)-> oldValue += newValue));
     }
 
     public int quiz3() throws IOException {
         List<String[]> csvLines = readCsvLines();
+
+        String findWord = "좋아";
+
+        return csvLines.stream()
+                .map(line -> countContains(line[2],0,findWord))
+                .reduce(0, Integer::sum);
+    }
+
+    private int countContains(String str, int fromIndex , String findWord ){
+        int findWordLength = findWord.length();
+        int index = str.indexOf(findWord, fromIndex);
+
+        if( index >= 0){
+            return 1 + countContains(str, index + findWordLength , findWord);
+        }
+
         return 0;
     }
 
