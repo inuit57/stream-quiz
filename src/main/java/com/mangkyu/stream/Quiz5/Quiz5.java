6d0e5e7a52f4b221ef92f063071d94b868a85c1e
inuit57 <inuit57@naver.com>
2022-07-18 13:44:55 +0900

    feat : 퀴즈 5 학습

--- src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
+++ src/main/java/com/mangkyu/stream/Quiz5/Quiz5.java
@@ -1,18 +1,26 @@
 package com.mangkyu.stream.Quiz5;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Quiz5 {
 
     private static final String[] STRING_ARR = {"aaa", "bb", "c", "dddd"};
 
     public int quiz1() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+                .collect(Collectors.joining())
+                .length();
     }
 
     public int quiz2() {
-        return 0;
+        return Arrays.stream(STRING_ARR)
+                .map(String::length)
+                .reduce(Integer::max)
+                .orElse(-1);
     }
 
     public List<Integer> quiz3() {
