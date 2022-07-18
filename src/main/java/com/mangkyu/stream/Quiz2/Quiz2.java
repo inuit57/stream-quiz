e7f7bc4bb7b8fbdc6997e2b6623debe9a68cd23e
inuit57 <inuit57@naver.com>
2022-07-18 12:53:08 +0900

    퀴즈 2 학습

--- src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
+++ src/main/java/com/mangkyu/stream/Quiz2/Quiz2.java
@@ -4,17 +4,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Quiz2 {
 
     private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
 
     public Map<String, Integer> quiz1() {
-        return new HashMap<>();
+
+        return WORDS.stream()
+                .map(word -> word.substring(0,1))
+                .collect(Collectors.toMap(prefix->prefix, prefix -> 1, (oldValue,newValue)-> oldValue+=newValue));
     }
 
     public String quiz2() {
-        return null;
+
+        return WORDS.stream()
+                .filter(word -> word.length() >= 2)
+                .map(word -> word.toUpperCase().substring(0,1))
+                .collect(Collectors.joining(" "));
     }
 
 }
